b168ea8866843a44b2e10a56328b65174a232362
Phuoc Nguyen <sapnkp@gmail.com>
2016-03-28 20:42:09 +0700

    Bỏ check token

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace App\Http\Middleware;
-
+use Closure;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
+use Illuminate\Session\TokenMismatchException;
+
 
 class VerifyCsrfToken extends BaseVerifier
 {
@@ -13,5 +15,23 @@ class VerifyCsrfToken extends BaseVerifier
      */
     protected $except = [
         //
+    	'test'
     ];
+
+    
+    // protected $except_urls = [
+    //     'test'
+    // ];
+
+    // public function handle($request, Closure $next)
+    // {
+    //     $regex = '#' . implode('|', $this->except_urls) . '#';
+
+    //     if ($this->isReading($request) || $this->tokensMatch($request) || preg_match($regex, $request->path()))
+    //     {
+    //         return $this->addCookieToResponse($request, $next($request));
+    //     }
+
+    //     throw new TokenMismatchException;
+    // }
 }
